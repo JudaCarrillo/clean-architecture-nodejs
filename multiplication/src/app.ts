425bebd133fdd2468ca58ef0e098b8e8a6b1f721
Juda <130939221+JudaCarrillo@users.noreply.github.com>
2024-06-08 00:51:17 -0500

    yargs configuration

--- multiplication/src/app.ts
+++ multiplication/src/app.ts
@@ -1,5 +1,13 @@
 import { yarg } from "./config/plugins/args.adapter";
 
-console.log(process.argv);
-console.log(yarg.b);
+// console.log(process.argv);
+// console.log(yarg.b);
 
+// * anonymous function - self invoking
+(async () => {
+  await main();
+})();
+
+async function main() {
+  console.log(yarg);
+}
--- multiplication/src/config/plugins/args.adapter.ts
+++ multiplication/src/config/plugins/args.adapter.ts
@@ -1,4 +1,29 @@
-import yargs from "yargs";
+import yargs, { check } from "yargs";
 import { hideBin } from "yargs/helpers";
 
-export const yarg = yargs(hideBin(process.argv)).parseSync();
+export const yarg = yargs(hideBin(process.argv))
+  .options({
+    b: {
+      alias: "base",
+      type: "number",
+      demandOption: true,
+      describe: "Base of the multiplication table",
+    },
+    l: {
+      alias: "limit",
+      type: "number",
+      default: 10,
+      describe: "Limit of the multiplication table",
+    },
+    s: {
+      alias: "show",
+      type: "boolean",
+      default: false,
+      describe: "Show the multiplication table",
+    },
+  })
+  .check((argv, options) => {
+    if (argv.b < 1) throw "Error: base must be greater than 0";
+    return true;
+  })
+  .parseSync();
