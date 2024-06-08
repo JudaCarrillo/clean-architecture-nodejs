44375bbef573dbad9ff4a6648ef6cb1fb40581f5
Juda <130939221+JudaCarrillo@users.noreply.github.com>
2024-06-08 00:35:56 -0500

    yargs configuration

--- multiplication/src/app.ts
+++ multiplication/src/app.ts
@@ -1,21 +1,5 @@
-import fs from "fs";
+import { yarg } from "./config/plugins/args.adapter";
 
-let outputMessage = "";
-const base = 5;
-const headerMessage = `
-========================================
-            Tabla del ${base}
-========================================\n
-`;
+console.log(process.argv);
+console.log(yarg.b);
 
-for (let i = 1; i <= 10; i++) {
-  outputMessage += `${base} x ${i} = ${base * i}\n`;
-}
-
-outputMessage = headerMessage + outputMessage;
-
-console.log(outputMessage);
-
-const outputPath = `outputs`;
-fs.mkdirSync(outputPath, { recursive: true });
-fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage);
