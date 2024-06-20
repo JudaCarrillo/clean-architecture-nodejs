4b3c701101479a6a69ea2a61e1e07d32b64f2564
Juda <130939221+JudaCarrillo@users.noreply.github.com>
2024-06-19 22:19:25 -0500

    use case testing completed

--- multiplication/src/domain/use-cases/save-file.use-case.test.ts
+++ multiplication/src/domain/use-cases/save-file.use-case.test.ts
@@ -1,15 +1,56 @@
+
+import fs from 'fs';
 import { SaveFile } from "./save-file.use-case";
 
 describe('save-file.use-case', () => {
 
+  const customOptions = {
+    fileContent: 'custom content',
+    fileDestination: 'custom-outputs/file-destination',
+    fileName: 'custom-table-name',
+  }
+
+  const customFilePath = `${customOptions.fileDestination}/${customOptions.fileName}.txt`;
+
+  afterEach(() => {
+
+    const outputFolderExists = fs.existsSync('outputs');
+    if (outputFolderExists) fs.rmSync('outputs', { recursive: true });
+
+    const customOutputFolderExists = fs.existsSync(customOptions.fileDestination);
+    if (customOutputFolderExists) fs.rmSync(customOptions.fileDestination, { recursive: true });
+
+  });
+
   it('should save file with default values', () => {
 
     const saveFile = new SaveFile();
+    const filePath = 'outputs/table.txt';
     const options = {
       fileContent: 'test content',
     }
 
-    saveFile.execute(options);
+    const result = saveFile.execute(options);    
+    const fileExists = fs.existsSync(filePath);
+    const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' });
+    
+    expect( result ).toBe(true);
+    expect( fileExists ).toBe(true);
+    expect( fileContent ).toBe(options.fileContent);
+
+  });
+
+  it('should save file with custom values', () => {
+
+    const saveFile = new SaveFile();
+
+    const result = saveFile.execute(customOptions);
+    const fileExists = fs.existsSync(customFilePath);
+    const fileContent = fs.readFileSync(customFilePath, { encoding: 'utf8' });
+
+    expect( result ).toBe(true);
+    expect( fileExists ).toBe(true);
+    expect( fileContent ).toBe(customOptions.fileContent);
 
   });
 
